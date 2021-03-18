66bf287fc928b915e8c4d89cc097967cfadbecad
Sergei Koldaev <s.koldaev@haulmont.com>
2021-03-18 15:27:05 +0300

    remove local addon

--- modules/core/test/com/company/uberjar/UberjarTestContainer.java
+++ modules/core/test/com/company/uberjar/UberjarTestContainer.java
@@ -12,8 +12,7 @@ public class UberjarTestContainer extends TestContainer {
         super();
         //noinspection ArraysAsListWithZeroOrOneArgument
         appComponents = Arrays.asList(
-                "com.haulmont.cuba",
-                "com.haulmont.addon.cubajm");
+                "com.haulmont.cuba");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the core module
--- modules/web/test/com/company/uberjar/UberjarWebTestContainer.java
+++ modules/web/test/com/company/uberjar/UberjarWebTestContainer.java
@@ -9,8 +9,7 @@ public class UberjarWebTestContainer extends TestContainer {
 
     public UberjarWebTestContainer() {
         appComponents = Arrays.asList(
-                "com.haulmont.cuba",
-                "com.haulmont.addon.cubajm");
+                "com.haulmont.cuba");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the web module
